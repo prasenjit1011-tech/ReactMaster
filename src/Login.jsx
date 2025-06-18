d7d6843ce972c30af1c4d028081449291e3e70ae
Prasenjit <prasenjit.aluni@gmail.com>
2025-06-18 10:16:53 +0530

    Commit

--- src/Login.jsx
+++ src/Login.jsx
@@ -2,7 +2,8 @@ import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { loginUser, logoutUser } from './myreduxthunk'
 import './App.css'
-function Login() {
+
+const Login = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const dispatch = useDispatch()
