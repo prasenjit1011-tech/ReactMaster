fab9af916403d5d479dcfc3479092bdd73b23e62
Prasenjit <prasenjit.aluni@gmail.com>
2025-06-18 08:50:58 +0530

    Redux Toolkit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import { useSelector, useDispatch } from 'react-redux';
+import { increment } from './store_reduxtoolkit';
 
+const App = () =>{
+    const txt       = `App.js : Vite + React`;
+    const count     = useSelector((state) => state.mycounter);
+    const dispatch  = useDispatch();
+    return (
+      <>
+        <button onClick={() => dispatch(increment())}>Increment</button>
+        <h3>{txt} : {count}</h3>
+        <CompA />
+        <CompB />
+      </>
+    )
+}
+
+const CompA = () => {
+  const txt   = `CompA : Vite + React`;
+  const cntA  = useSelector(state => state.mycounter);
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h3>{txt} : {cntA}</h3>
     </>
   )
 }
 
-export default App
+const CompB = () => {
+  const txt   = `CompB : Vite + React`;
+  const cntB  = useSelector(state => state.mycounter);
+  return (
+    <>
+      <h3>{txt} : {cntB}</h3>
+    </>
+  )
+}
+
+
+
+export default App;
