33031a95cef355497cbeeb47a0a4c9fd225ab607
Mr. Aluni <prasenjit.aluni@gmail.com>
2026-06-03 08:04:18 +0530

    Org44

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,35 +1,34 @@
 resource "aws_amplify_app" "react_app" {
-  name         = "reactmaster"
-  repository   = "https://github.com/prasenjit1011-tech/ReactMaster"
-  # access_token = var.github_token
+  name       = var.app_name
+  repository = var.repository_url
 
   platform = "WEB"
 
-  build_spec = <<EOF
-version: 1
-frontend:
-  phases:
-    preBuild:
-      commands:
-        - npm ci
-    build:
-      commands:
-        - npm run build
-  artifacts:
-    baseDirectory: dist
-    files:
-      - '**/*'
-  cache:
-    paths:
-      - node_modules/**/*
-EOF
+  build_spec = <<-EOF
+    version: 1
+    frontend:
+      phases:
+        preBuild:
+          commands:
+            - npm ci
+        build:
+          commands:
+            - npm run build
+      artifacts:
+        baseDirectory: dist
+        files:
+          - '**/*'
+      cache:
+        paths:
+          - node_modules/**/*
+  EOF
 
   enable_branch_auto_build = true
 }
 
 resource "aws_amplify_branch" "main" {
   app_id      = aws_amplify_app.react_app.id
-  branch_name = "amplify_main"
+  branch_name = var.branch_name
 
   framework = "React"
   stage     = "PRODUCTION"
