741e96d9b66299b42f61b3ffb3214acd6de1d6a5
Mr. Aluni <prasenjit.aluni@gmail.com>
2026-06-03 08:15:36 +0530

    Org45

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,28 +1,28 @@
 resource "aws_amplify_app" "react_app" {
-  name       = var.app_name
-  repository = var.repository_url
+  name         = var.app_name
+  repository   = var.repository_url
   access_token = var.github_token
 
   platform = "WEB"
 
   build_spec = <<-EOF
-    version: 1
-    frontend:
-      phases:
-        preBuild:
-          commands:
-            - npm ci
-        build:
-          commands:
-            - npm run build
-      artifacts:
-        baseDirectory: dist
-        files:
-          - '**/*'
-      cache:
-        paths:
-          - node_modules/**/*
-  EOF
+version: 1
+frontend:
+  phases:
+    preBuild:
+      commands:
+        - npm ci
+    build:
+      commands:
+        - npm run build
+  artifacts:
+    baseDirectory: dist
+    files:
+      - '**/*'
+  cache:
+    paths:
+      - node_modules/**/*
+EOF
 
   enable_branch_auto_build = true
 }
@@ -31,8 +31,7 @@ resource "aws_amplify_branch" "main" {
   app_id      = aws_amplify_app.react_app.id
   branch_name = var.branch_name
 
-  framework = "React"
-  stage     = "PRODUCTION"
-
+  framework        = "React"
+  stage            = "PRODUCTION"
   enable_auto_build = true
 }
