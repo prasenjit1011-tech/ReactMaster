4fc6dfe83e9b564317d5fc54bcce823abba5049d
Mr. Aluni <prasenjit.aluni@gmail.com>
2026-06-05 08:50:39 +0530

    222

--- terraform/main.tf
+++ terraform/main.tf
@@ -44,7 +44,7 @@ resource "google_project_iam_member" "artifact_writer" {
 # Artifact Registry (Docker Repo)
 # -----------------------------
 resource "google_artifact_registry_repository" "docker_repo" {
-  project       = var.project_id
+  project       = "terraform-497011"
   location      = "asia-south1"
   repository_id = "react-app"
   description   = "Docker repo for React app"
@@ -96,13 +96,3 @@ resource "google_cloud_run_v2_service_iam_member" "public" {
   member = "allUsers"
 }
 
-resource "google_artifact_registry_repository" "docker_repo" {
-  project       = var.project_id
-  location      = "asia-south1"
-  repository_id = "react-app"
-  format        = "DOCKER"
-
-  depends_on = [
-    google_project_service.artifact_registry_api
-  ]
-}
