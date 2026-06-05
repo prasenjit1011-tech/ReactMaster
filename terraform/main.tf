accd7e60dfdc95a533461fb479f9b70156eb4bcc
Mr. Aluni <prasenjit.aluni@gmail.com>
2026-06-05 13:30:29 +0530

    225

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,7 +20,7 @@ resource "google_project_iam_member" "artifact_writer" {
 
 resource "google_cloud_run_v2_service" "app" {
   project  = "terraform-497011"
-  name     = "react-cloudrun-v2"
+  name     = "react-cloudrun-v3"
   location = "asia-south1"
 
   template {
