ec78ec7a16c2892ee10ab3fb6e192022b4181674
Mr. Aluni <prasenjit.aluni@gmail.com>
2026-06-08 08:46:23 +0530

    AWS Amplify

--- src/About.tsx
+++ src/About.tsx
@@ -38,7 +38,7 @@ const About = () => {
 
             {/* Azure */}
             <a
-            href="#"
+            href="https://happy-dune-08b101900.7.azurestaticapps.net/"
             rel="noreferrer"
             className="px-6 py-3 rounded-xl border border-sky-500/40 text-sky-400 hover:bg-sky-500/10 transition"
             >
@@ -47,7 +47,7 @@ const About = () => {
 
             {/* Portfolio - GCP Cloud Run */}
             <a
-            href="https://prasenjit1011.netlify.app/"
+            href="https://j4t3c8u9u1.execute-api.us-east-1.amazonaws.com/"
             rel="noreferrer"
             className="px-6 py-3 rounded-xl text-emerald-300 border border-emerald-500/40 hover:border-emerald-400 transition font-medium"
             >
